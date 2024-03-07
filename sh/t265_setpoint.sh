49e8098f4da74fb645408b39879d4c5d8540265e
maxibooksiyi <maxi1372740910@foxmail.com>
2024-03-07 10:44:42 +0800

    Update t265_setpoint.sh
    改为相对路径

--- sh/t265_setpoint.sh
+++ sh/t265_setpoint.sh
@@ -1,8 +1,8 @@
 gnome-terminal --window -e 'bash -c " roslaunch realsense2_camera rs_t265.launch; exec bash"' \
---tab -e 'bash -c "sleep 15; source ~/maxitest_ws/devel/setup.bash;rosrun offboard_pkg t265_to_mavros; exec bash"' \
+--tab -e 'bash -c "sleep 15; source ../../../devel/setup.bash;rosrun offboard_pkg t265_to_mavros; exec bash"' \
 --tab -e 'bash -c "sleep 16; ./ttyTHS0.sh; exec bash"' \
 --tab -e 'bash -c "sleep 18; roslaunch mavros px4.launch; exec bash"' \
---tab -e 'bash -c "sleep 20; source ~/maxitest_ws/devel/setup.bash;roslaunch offboard_pkg setpoint.launch; exec bash"' \
+--tab -e 'bash -c "sleep 20; source ../../../devel/setup.bash;roslaunch offboard_pkg setpoint.launch; exec bash"' \
 --window -e 'bash -c "sleep 25; rostopic echo /mavros/local_position/pose; exec bash"' \
 --window -e 'bash -c "sleep 25; rostopic echo /mavros/state; exec bash"' \
 --window -e 'bash -c "sleep 25; rostopic echo mavros/setpoint_position/local; exec bash"' \
